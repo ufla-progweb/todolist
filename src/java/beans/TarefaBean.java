0781792bf507bb0e444cb17a836cf95a9005a8d9
Paulo <pauloa.junior@dcc.ufla.br>
2016-02-18 08:38:41 -0200

    voltando para a versão de lista

--- src/java/beans/TarefaBean.java
+++ src/java/beans/TarefaBean.java
@@ -5,84 +5,41 @@
  */
 package beans;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
-import javax.annotation.PostConstruct;
 import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
 import javax.faces.context.FacesContext;
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.EntityTransaction;
-import javax.persistence.Persistence;
 import model.Tarefa;
 
 @ManagedBean
 @ViewScoped
-public class TarefaBean {
+public class TarefaBean implements Serializable {
 
     public static final String PR_ALTA = "Alta";
     public static final String PR_MEDIA = "Média";
     public static final String PR_BAIXA = "Baixa";
-    public static final String TODOLIST_PU = "TodoListPU";
 
-    private List<Tarefa> tarefas;
+    private List<Tarefa> tarefas = new ArrayList<Tarefa>();
     private Tarefa novaTarefa = new Tarefa();
 
-    @PostConstruct
-    public void init() {
-        tarefas = new ArrayList<Tarefa>();
-        tarefas.add(new Tarefa("Tarefa 1", "Alta"));
-        tarefas.add(new Tarefa("Tarefa 2", "Média"));
-        tarefas.add(new Tarefa("Tarefa 3", "Baixa"));
-        tarefas.add(new Tarefa("Tarefa 4", "Alta"));
-    }
-
     public void salvar() {
-        EntityManagerFactory emf = Persistence.
-                createEntityManagerFactory(TODOLIST_PU);
-        EntityManager em = emf.createEntityManager();
-
-        EntityTransaction tx = em.getTransaction();
-        tx.begin();
-        em.persist(novaTarefa);
-        tx.commit();
-        em.close();
-        emf.close();
-
-        novaTarefa = new Tarefa();
-        FacesContext.getCurrentInstance().addMessage(null,
-                new FacesMessage(FacesMessage.SEVERITY_INFO,
-                        "Tarefa cadastrada com sucesso!", null));
+        FacesMessage mensagem;
+        if (!tarefas.contains(novaTarefa)) {
+            tarefas.add(novaTarefa);
+            novaTarefa = new Tarefa();
+            mensagem = new FacesMessage(FacesMessage.SEVERITY_INFO,
+                    "Tarefa cadastrada com sucesso!", null);
+        } else {
+            mensagem = new FacesMessage(FacesMessage.SEVERITY_ERROR,
+                    "Não pode haver duas tarefas com mesma descrição e deadline!",
+                    null);
+        }
+        FacesContext.getCurrentInstance().addMessage(null, mensagem);
     }
 
-//    public void salvar() {
-//        EntityManagerFactory emf = Persistence.
-//            createEntityManagerFactory(TODOLIST_PU);
-//        EntityManager em = emf.createEntityManager();
-//        
-//        FacesMessage mensagem;
-//        if (!tarefas.contains(novaTarefa)) {
-//            
-//            EntityTransaction tx = em.getTransaction();
-//            tx.begin();
-//            em.persist(novaTarefa);
-//            tx.commit();
-//            
-//            novaTarefa = new Tarefa();
-//            mensagem = new FacesMessage(FacesMessage.SEVERITY_INFO,
-//                    "Tarefa cadastrada com sucesso!", null);
-//        } else {
-//            mensagem = new FacesMessage(FacesMessage.SEVERITY_ERROR,
-//                    "Não pode haver duas tarefas com mesma descrição e deadline!",
-//                    null);
-//        }
-//        FacesContext.getCurrentInstance().addMessage(null, mensagem);
-//        
-//        em.close();
-//        emf.close();
-//    }
     public void concluir(Tarefa tarefa) {
         tarefas.remove(tarefa);
         FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO,
@@ -100,20 +57,6 @@ public class TarefaBean {
 
     public void setTarefas(List<Tarefa> tarefas) {
         this.tarefas = tarefas;
-    }
-
-    public List<String> completarPrioridade(String entrada) {
-        List<String> resultados = new ArrayList<String>();
-        if (PR_ALTA.contains(entrada)) {
-            resultados.add(PR_ALTA);
-        }
-        if (PR_MEDIA.contains(entrada)) {
-            resultados.add(PR_MEDIA);
-        }
-        if (PR_BAIXA.contains(entrada)) {
-            resultados.add(PR_BAIXA);
-        }
-        return resultados;
     }
 
     public Tarefa getNovaTarefa() {
